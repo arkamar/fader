456dd8952109ef17e185827110033f418b9cf849
Petr Vaněk <arkamar@atlas.cz>
2020-03-08 14:55:43 +0100

    fader: introduce simple client for backlight control

--- fader.c
+++ fader.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,12 +9,41 @@
 
 #include "ipc.h"
 
+void
+usage(const char * argv0) {
+		fputs("fader cmd\n", stderr);
+		exit(1);
+}
+
 int
 main(int argc, char * argv[]) {
 	struct ipc ipc;
+	enum fader_type type = FADER_SET;
+	int value;
+
+	char * argv0 = *argv; argv++; argc--;
+
+	if (argc < 1) {
+		usage(argv0);
+	}
+
+	const char * cmd = *argv; argv++; argc--;
+	char * end;
+
+	value = strtol(cmd, &end, 0);
+
+	if (cmd[0] == '+' || cmd[0] == '-') {
+		type = FADER_INC;
+		if (*end == '%') {
+			type = FADER_INC_PERC;
+		}
+	} else if (*end == '%') {
+		type = FADER_SET_PERC;
+	}
+
 	struct msg msg = {
-		.type = FADER_NOP,
-		.value = 0,
+		.type = type,
+		.value = value,
 	};
 
 	ipc_connect(&ipc);
